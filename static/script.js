8e0cb9df7f19994d0a4468eb119c40bd17eeda94
SunnyHuangCodebase <106368104+SunnyHuangCodebase@users.noreply.github.com>
2023-03-08 00:00:55 -0500

    Implement CSRF Token
    Update HTTP Request AJAX calls to use CSRF tokens if they exist.
    
    Extend base index.html with CSRF token block in django.html

--- static/script.js
+++ static/script.js
@@ -1,16 +1,57 @@
+function get_cookie(name) {
+    let cookieValue = null
+    if (document.cookie && document.cookie !== "") {
+        const cookies = document.cookie.split(';')
+        for (let i = 0; i < cookies.length; i ++) {
+            const cookie = cookies[i].trim()
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1))
+                break
+            }
+        }
+    }
+    console.log(cookieValue)
+    return cookieValue
+}
+async function add_task(form){
+    csrftoken = get_cookie("csrftoken")
+    console.log(csrftoken)
+    url = "/add"
+
+    fetch(form.action, {method: form.method, body: new FormData(form)})
+    .then(() => location.reload())
+}
 async function update_task(button){
-    url = `/update/${button.value}`
-    fetch(url, {method: "PATCH"})
+    csrftoken = get_cookie("csrftoken")
+    url = `/update/${button.value}/`
+    fetch(
+        url, {
+            method: "PATCH",
+            headers: {"X-CSRFTOKEN": csrftoken}
+        }
+    )
     .then(() => location.reload())
 }
 
 async function delete_task(button){
-    url = `/delete/${button.value}`
-    fetch(url, {method: "DELETE"})
+    csrftoken = get_cookie("csrftoken")
+    url = `/delete/${button.value}/`
+    fetch(
+        url, {
+            method: "DELETE",
+            headers: {"X-CSRFTOKEN": csrftoken}
+        }
+    )
     .then(() => location.reload())
 }
 async function delete_all_tasks(){
-    url = "/delete_all"
-    fetch(url, {method: "DELETE"})
+    csrftoken = get_cookie("csrftoken")
+    url = "/delete_all/"
+    fetch(
+        url, {
+            method: "DELETE",
+            headers: {"X-CSRFTOKEN": csrftoken}
+        }
+    )
     .then(() => location.reload())
 }
