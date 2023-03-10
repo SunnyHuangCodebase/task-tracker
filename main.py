53fe8826c91f0c7123c7ffc2f44cc61893b5a423
SunnyHuangCodebase <106368104+SunnyHuangCodebase@users.noreply.github.com>
2023-03-09 21:20:23 -0500

    Add Command Line Arguments to main.py
    Allow use to specify which app to run via command line using app name or ID.

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
+import sys
+
 from django_app.app import main as run_django
 from fastapi_app.app import main as run_fastapi
 from flask_app.app import main as run_flask
 
-app = {
-    1: run_flask,
-    2: run_fastapi,
-    3: run_django
+APPS = {
+    "django": run_django,
+    "fastapi": run_fastapi,
+    "flask": run_flask,
+    "1": run_django,
+    "2": run_fastapi,
+    "3": run_flask,
 }
 
-def main(app_id: int):
-    app[app_id]()
+DEFAULT_APP = 1
+
+def main(app_id: str):
+    APPS[app_id]()
 
 
 if __name__ == "__main__":
-    APP_ID = 3
-    main(APP_ID)
+    app = sys.argv[-1] if sys.argv[-1] in APPS else str(DEFAULT_APP)
+    main(app)
