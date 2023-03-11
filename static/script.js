c41902875f121148daf591ee94c7a472f5589019
SunnyHuangCodebase <106368104+SunnyHuangCodebase@users.noreply.github.com>
2023-03-10 19:41:59 -0500

    Update AJAX Requests
    Remove redundant reload on AJAX requests.
    
    Add functions to retrieve CSRF token in cookies.

--- static/script.js
+++ static/script.js
@@ -12,44 +12,59 @@ function get_cookie(name) {
     }
     return cookieValue
 }
+
+function set_cookie(name, value, time = 5) {
+    let expiry = new Date()
+    expiry.setTime(Date.now() + time * 60 * 1000)
+    document.cookie = name + "=" + value + "; expires=" + expiry.toUTCString();
+}
+
+function delete_cookie(name) {
+    set_cookie(name, value, 0)
+}
+
+function get_csrftoken() {
+    return get_cookie("csrftoken")
+}
+
 async function add_task(form){
-    csrftoken = get_cookie("csrftoken")
-    url = "/add"
-
-    fetch(form.action, {method: form.method, body: new FormData(form)})
-    .then(() => location.reload())
-}
-async function update_task(button){
-    csrftoken = get_cookie("csrftoken")
-    url = `/update/${button.value}`
-    fetch(
-        url, {
-            method: "PATCH",
-            headers: {"X-CSRFTOKEN": csrftoken}
-        }
-    )
-    .then(() => location.reload())
+    fetch(form.action, {
+        method: form.method,
+        body: new FormData(form),
+        headers: {"X-CSRFTOKEN": get_csrftoken()}
+    })
+    .then(response => response.text())
+    .then(html => document.body.innerHTML = html)
 }
 
-async function delete_task(button){
-    csrftoken = get_cookie("csrftoken")
-    url = `/delete/${button.value}`
-    fetch(
-        url, {
-            method: "DELETE",
-            headers: {"X-CSRFTOKEN": csrftoken}
-        }
-    )
-    .then(() => location.reload())
+async function update_task(button) {
+    let token = get_csrftoken()
+    console.log(token)
+    let url = `/update/${button.value}`
+    fetch(url, {
+        method: "PATCH",
+        headers: {"X-CSRFTOKEN": get_csrftoken()}
+    })
+    .then(response => response.text())
+    .then(html => document.body.innerHTML = html)
 }
-async function delete_all_tasks(){
-    csrftoken = get_cookie("csrftoken")
-    url = "/delete_all"
-    fetch(
-        url, {
-            method: "DELETE",
-            headers: {"X-CSRFTOKEN": csrftoken}
-        }
-    )
-    .then(() => location.reload())
+
+async function delete_task(button) {
+    let url = `/delete/${button.value}`
+    fetch(url, {
+        method: "DELETE",
+        headers: {"X-CSRFTOKEN": get_csrftoken()}
+    })
+    .then(response => response.text())
+    .then(html => document.body.innerHTML = html)
+}
+async function delete_all_tasks() {
+    let url = "/delete_all"
+    fetch(url, {
+        method: "DELETE",
+        headers: {"X-CSRFTOKEN": get_csrftoken()}
+    })
+    .then(response => response.text())
+    .then(html => document.body.innerHTML = html)
+
 }
