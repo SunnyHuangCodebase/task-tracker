018425f7aa88a89df5a819dd9638bea33c7b7e73
SunnyHuangCodebase <106368104+SunnyHuangCodebase@users.noreply.github.com>
2023-03-12 20:43:05 -0400

    Add CSRF Protection to FastAPI
    Add FastAPI CSRF Protect as dependency in Poetry.
    
    Configure CSRF Protection in FastAPI app.
    
    Add CSRF Token to FastAPI HTML template.

--- fastapi_app/app.py
+++ fastapi_app/app.py
@@ -1,6 +1,10 @@
+import os
+
 import uvicorn
 from fastapi import Depends, FastAPI, Form, Request, status
 from fastapi.staticfiles import StaticFiles
+from fastapi_csrf_protect import CsrfProtect
+from pydantic import BaseModel
 from starlette.responses import RedirectResponse
 from starlette.templating import Jinja2Templates
 
@@ -18,6 +22,19 @@ class FastAPIApp(FastAPI):
         self.database = database
 
 
+SECRET_KEY = str(os.urandom(32))
+
+
+class CSRFSettings(BaseModel):
+    secret_key: str = SECRET_KEY
+    csrf_header_name: str = "X-CSRFTOKEN"
+
+
+@CsrfProtect.load_config
+def get_csrf_config():
+    return CSRFSettings()
+
+
 app = FastAPIApp()
 app.set_database(Database())
 app.mount("/static", StaticFiles(directory="./static"), name="static")
@@ -28,27 +45,41 @@ def get_db():
 
 
 @app.get("/")
-async def home(request: Request, db: Database = Depends(get_db)):
+async def home(request: Request,
+               db: Database = Depends(get_db),
+               csrf_protect: CsrfProtect = Depends()):
     tasks = Task.get_all(db)
     in_progress = [task for task in tasks if not task.complete]
     complete = [task for task in tasks if task.complete]
-    return templates.TemplateResponse("fastapi.html", {
-        "request": request,
-        "in_progress": in_progress,
-        "complete": complete,
-    })
+    csrf_token = csrf_protect.generate_csrf()
+    return templates.TemplateResponse(
+        "fastapi.html", {
+            "request": request,
+            "in_progress": in_progress,
+            "complete": complete,
+            "csrf_token": csrf_token
+        })
 
 
 @app.post("/add")
-def add(request: Request, name: str = Form(...),
-        db: Database = Depends(get_db)):
+def add(request: Request,
+        name: str = Form(...),
+        db: Database = Depends(get_db),
+        csrf_protect: CsrfProtect = Depends()):
+    csrf_token = csrf_protect.get_csrf_from_headers(request.headers)
+    csrf_protect.validate_csrf(csrf_token)
     Task(name, db)
     url = app.url_path_for("home")
     return RedirectResponse(url=url, status_code=status.HTTP_303_SEE_OTHER)
 
 
 @app.patch("/update/{task_id}")
-def update(request: Request, task_id: int, db: Database = Depends(get_db)):
+def update(request: Request,
+           task_id: int,
+           db: Database = Depends(get_db),
+           csrf_protect: CsrfProtect = Depends()):
+    csrf_token = csrf_protect.get_csrf_from_headers(request.headers)
+    csrf_protect.validate_csrf(csrf_token)
     task = Task.get_by_id(db, task_id)
     task.toggle_complete()
 
@@ -57,7 +88,12 @@ def update(request: Request, task_id: int, db: Database = Depends(get_db)):
 
 
 @app.delete("/delete/{task_id}")
-def delete(request: Request, task_id: int, db: Database = Depends(get_db)):
+def delete(request: Request,
+           task_id: int,
+           db: Database = Depends(get_db),
+           csrf_protect: CsrfProtect = Depends()):
+    csrf_token = csrf_protect.get_csrf_from_headers(request.headers)
+    csrf_protect.validate_csrf(csrf_token)
     task = Task.get_by_id(db, task_id)
     task.delete()
     url = app.url_path_for("home")
@@ -65,7 +101,9 @@ def delete(request: Request, task_id: int, db: Database = Depends(get_db)):
 
 
 @app.delete("/delete_all")
-async def delete_all():
+async def delete_all(request: Request, csrf_protect: CsrfProtect = Depends()):
+    csrf_token = csrf_protect.get_csrf_from_headers(request.headers)
+    csrf_protect.validate_csrf(csrf_token)
     Task.delete_all(app.database)
     url = app.url_path_for("home")
     return RedirectResponse(url=url, status_code=status.HTTP_303_SEE_OTHER)
